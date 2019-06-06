a8d40d0c0df322b29058a3cc8c6ef193dae3a881
guolinhao <gavin141113@163.com>
2019-06-07 00:02:00 +0800

    彩色小球弹跳效果

--- countdown.js
+++ countdown.js
@@ -7,6 +7,11 @@ var MARGIN_LEFT = 30;
 var endTime = new Date(2019,5,8,12,0,0);
 var curShowTimeSeconds = 0;
 
+var balls = [];
+const colors = ["#33B5E5","#0099CC","#AA66CC","#9933CC","#99CC00","#669900","#FFBB33","#FF8800","#FF4444","#CC0000"];
+
+
+
 window.onload = function() {
 
     var canvas = document.getElementById('canvas');
@@ -41,8 +46,70 @@ function update() {
     var curSeconds = curShowTimeSeconds % 60;
 
     if (nextSeconds !== curSeconds) {
+        if( parseInt(curHours/10) !== parseInt(nextHours/10) ){
+            addBalls( MARGIN_LEFT + 0 , MARGIN_TOP , parseInt(curHours/10) );
+        }
+        if( parseInt(curHours%10) !== parseInt(nextHours%10) ){
+            addBalls( MARGIN_LEFT + 15*(RADIUS+1) , MARGIN_TOP , parseInt(curHours/10) );
+        }
+
+        if( parseInt(curMinutes/10) !== parseInt(nextMinutes/10) ){
+            addBalls( MARGIN_LEFT + 39*(RADIUS+1) , MARGIN_TOP , parseInt(curMinutes/10) );
+        }
+        if( parseInt(curMinutes%10) !== parseInt(nextMinutes%10) ){
+            addBalls( MARGIN_LEFT + 54*(RADIUS+1) , MARGIN_TOP , parseInt(curMinutes%10) );
+        }
+
+        if( parseInt(curSeconds/10) !== parseInt(nextSeconds/10) ){
+            addBalls( MARGIN_LEFT + 78*(RADIUS+1) , MARGIN_TOP , parseInt(curSeconds/10) );
+        }
+        if( parseInt(curSeconds%10) !== parseInt(nextSeconds%10) ){
+            addBalls( MARGIN_LEFT + 93*(RADIUS+1) , MARGIN_TOP , parseInt(nextSeconds%10) );
+        }
+
         curShowTimeSeconds = nextShowTimeSeconds;
     }
+
+    updateBalls();
+}
+
+function updateBalls() {
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].x += balls[i].vx;
+        balls[i].y += balls[i].vy;
+        balls[i].vy += balls[i].g;
+
+        // 碰撞检测
+        // 落地反弹
+        if (balls[i].y > WINDOW_HEIGHT - RADIUS) {
+            balls[i].y = WINDOW_HEIGHT - RADIUS;
+            balls[i].vy = -balls[i].vy*0.6
+        }
+        // 右壁反弹
+        if (balls[i].x > WINDOW_WIDTH - RADIUS) {
+            balls[i].x = WINDOW_WIDTH - RADIUS;
+            balls[i].vx = -balls[i].vx;
+        }
+        
+    }
+}
+
+function addBalls( x , y , num ){
+    for( var i = 0  ; i < digit[num].length ; i ++ ) {
+        for( var j = 0  ; j < digit[num][i].length ; j ++ ) {
+            if( digit[num][i][j] == 1 ){
+                var aBall = {
+                    x: x+j*2*(RADIUS+1)+(RADIUS+1),
+                    y: y+i*2*(RADIUS+1)+(RADIUS+1),
+                    g: 1+Math.random(),
+                    vx: Math.pow( -1 , Math.ceil( Math.random()*1000 ) ) * 4,
+                    vy: -5+2*Math.random(),
+                    color: colors[ Math.floor( Math.random()*colors.length ) ]
+                }
+                balls.push( aBall )
+            }
+        }    
+    }      
 }
 
 function render(cxt) {
@@ -61,6 +128,16 @@ function render(cxt) {
     renderDigit(MARGIN_LEFT+69*(RADIUS+1), MARGIN_TOP, 10, cxt);
     renderDigit(MARGIN_LEFT+78*(RADIUS+1), MARGIN_TOP, parseInt(seconds/10), cxt);
     renderDigit(MARGIN_LEFT+93*(RADIUS+1), MARGIN_TOP, parseInt(seconds%10), cxt);
+
+    for( var i = 0 ; i < balls.length ; i ++ ){
+        cxt.fillStyle=balls[i].color;
+
+        cxt.beginPath();
+        cxt.arc( balls[i].x , balls[i].y , RADIUS , 0 , 2*Math.PI , true );
+        cxt.closePath();
+
+        cxt.fill();
+    }
 }
 
 function renderDigit(x, y, num, cxt) {
